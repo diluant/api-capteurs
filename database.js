823eb0326fc8ceb69260295f384069138faa5452
Steve Morin <diluant@users.noreply.github.com>
2024-08-11 11:24:23 -0400

    Update database.js
    Ajout des commentaires en français

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
+// Connexion à la base de données SQLite (ou création si n'existe pas)
 // Connect to the SQLite database (or create it if it doesn't exist)
 const dbPath = path.resolve(__dirname, 'sensor_data.db');
 const db = new sqlite3.Database(dbPath);
 
+// Création de la table pour storer les données des capteurs si n'existe pas
 // Create the table for storing sensor data if it doesn't exist
 db.serialize(() => {
     db.run(`
