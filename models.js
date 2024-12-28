631a177190ebe5edfa4d36214cfd639054f57069
Steve Morin <diluant@users.noreply.github.com>
2024-12-27 22:44:05 -0500

    Update models.js

--- models.js
+++ models.js
@@ -1,12 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+
 class Coordonnee {
-    constructor(x, y, z, latitude, longitude, speed, trajetId) {
+    constructor(x, y, z, latitude, longitude, speed) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.latitude = latitude;
         this.longitude = longitude;
         this.speed = speed;
-        this.trajetId = trajetId; // Clé étrangère
     }
 
     static initialize(db) {
@@ -18,9 +19,7 @@ class Coordonnee {
                 z REAL NOT NULL,
                 latitude REAL NOT NULL,
                 longitude REAL NOT NULL,
-                speed REAL NOT NULL,
-                trajetId INTEGER,
-                FOREIGN KEY (trajetId) REFERENCES trajets(id) ON DELETE CASCADE
+                speed REAL NOT NULL
             );
         `, (err) => {
             if (err) {
@@ -35,8 +34,8 @@ class Coordonnee {
         return new Promise((resolve, reject) => {
             const db = new sqlite3.Database('./database.sqlite');
             db.run(
-                `INSERT INTO sensor_data (x, y, z, latitude, longitude, speed, trajetId) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-                [this.x, this.y, this.z, this.latitude, this.longitude, this.speed, this.trajetId],
+                `INSERT INTO sensor_data (x, y, z, latitude, longitude, speed) VALUES (?, ?, ?, ?, ?, ?)`,
+                [this.x, this.y, this.z, this.latitude, this.longitude, this.speed],
                 function (err) {
                     if (err) {
                         reject(err);
@@ -49,21 +48,73 @@ class Coordonnee {
         });
     }
 
-    static getAllByTrajetId(trajetId) {
+    static getAll() {
         return new Promise((resolve, reject) => {
             const db = new sqlite3.Database('./database.sqlite');
-            db.all(
-                `SELECT * FROM sensor_data WHERE trajetId = ?`,
-                [trajetId],
-                (err, rows) => {
+            db.all(`SELECT * FROM sensor_data`, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+            db.close();
+        });
+    }
+}
+
+class Trajet {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    static initialize(db) {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS trajets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            );
+        `, (err) => {
+            if (err) {
+                console.error("Erreur lors de la création de la table trajets:", err.message);
+            } else {
+                console.log("Table trajets prête.");
+            }
+        });
+    }
+
+    save() {
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database('./database.sqlite');
+            db.run(
+                `INSERT INTO trajets (name) VALUES (?)`,
+                [this.name],
+                function (err) {
                     if (err) {
                         reject(err);
                     } else {
-                        resolve(rows);
+                        resolve({ id: this.lastID, ...this });
                     }
                 }
             );
             db.close();
         });
     }
+
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database('./database.sqlite');
+            db.all(`SELECT * FROM trajets`, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+            db.close();
+        });
+    }
 }
+
+module.exports = { Coordonnee, Trajet };
